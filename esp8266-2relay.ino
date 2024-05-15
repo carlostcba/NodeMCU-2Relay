39da1802b4c499b6061207082eb6ce704bdf0fd2
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-05-15 11:28:03 -0300

    Update esp8266-2relay.ino

--- esp8266-2relay.ino
+++ esp8266-2relay.ino
@@ -57,6 +57,22 @@ void loop() {
 void handleRoot() {
   // Página principal con botones para controlar los Reles
   String html = "<html><head><title>Control de Reles</title>";
+  html += "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  html += "<style>";
+  html += "body { font-family: Calibri, sans-serif; text-align: center; background-color: #014a99; color: #f8f9fa; }";
+  html += "h1 { margin-top: 100px; }";
+  html += "h1 { margin-bottom: 50px; }";
+  html += "h3 { color: #f29419; }";
+  html += "h3 { margin-top: 50px; }";
+  html += "button { padding: 20px 40px; font-size: 20px; margin: 5px; background-color: #ffc107; border: none; border-radius: 8px; color: #f8f9fa; font-weight: bold; }";
+  html += "button:disabled { background-color: #6c757d; }";
+  html += "</style>";
+  html += "</head><body>";
+  html += "<h1>Acceso Yrigoyen</h1>";
+  html += "<h3>Porton</h3>";
+  html += "<button id='btn1' onclick='accionarRelay1()' " + String(btn1Enabled ? "" : "disabled") + ">Accionar</button>";
+  html += "<h3>Puerta</h3>";
+  html += "<button id='btn2' onclick='accionarRelay2()' " + String(btn2Enabled ? "" : "disabled") + ">Accionar</button>";
   html += "<script>";
   html += "function accionarRelay1() {";
   html += "if (" + String(btn1Enabled) + ") {";
@@ -69,7 +85,7 @@ void handleRoot() {
   html += "setTimeout(function() {";
   html += "btn1.innerHTML = 'Accionar';";
   html += "btn1.disabled = false;";
-  html += "}, 500);"; // Después de 500 ms, volver a habilitar el botón y cambiar el texto a "Accionar"
+  html += "}, 1000);"; // Después de 1000 ms, volver a habilitar el botón y cambiar el texto a "Accionar"
   html += "}";
   html += "}";
   html += "function accionarRelay2() {";
@@ -83,17 +99,10 @@ void handleRoot() {
   html += "setTimeout(function() {";
   html += "btn2.innerHTML = 'Accionar';";
   html += "btn2.disabled = false;";
-  html += "}, 500);"; // Después de 500 ms, volver a habilitar el botón y cambiar el texto a "Accionar"
+  html += "}, 600);"; // Después de 600 ms, volver a habilitar el botón y cambiar el texto a "Accionar"
   html += "}";
   html += "}";
-  html += "</script></head><body>";
-  html += "<h1>Control de Porton</h1>";
-  html += "<h2>Rele 1</h2>";
-  html += "<button id='btn1' onclick='accionarRelay1()'>Accionar</button>";
-  html += "<h1>Control de Puerta</h1>";
-  html += "<h2>Rele 2</h2>";
-  html += "<button id='btn2' onclick='accionarRelay2()'>Accionar</button>";
-  html += "</body></html>";
+  html += "</script></body></html>";
   server.send(200, "text/html", html);
 }
 
